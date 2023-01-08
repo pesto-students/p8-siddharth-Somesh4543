fade88bb373dadd0e8e25c952e46e166122da993
Somesh4543 <Somesh.4543@gmail.com>
2023-01-08 21:59:44 +0530

    Exercise4.3

--- Week-4/Excercise4.2.js
+++ Week-4/Excercise4.2.js
@@ -3,12 +3,12 @@ Person.prototype.initialize = function (name, age) {
   this.name = name;
   this.age = age;
 };
-var Teacher = function () {
+var teacher = function () {
   this.teach = function (subject) {
     console.log(this.name + "is now teaching" + subject);
   };
 };
-Teacher.prototype = new Person();
-var him = new Teacher();
+teacher.prototype = new Person();
+var him = new teacher();
 him.initialize("Adam", 45);
 him.teach("Inheritence");
